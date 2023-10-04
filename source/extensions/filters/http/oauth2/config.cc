cce80b1f8b00884e76f03d8b03547f9f1faf5366
huabing zhao <zhaohuabing@gmail.com>
2023-10-04 20:10:54 +0800

    move oauth2 config out
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>

--- source/extensions/filters/http/oauth2/config.cc
+++ source/extensions/filters/http/oauth2/config.cc
@@ -76,7 +76,7 @@ Http::FilterFactoryCb FilterFactory::createFilterFactoryFromProtoTyped(
         std::unique_ptr<OAuth2Client> oauth_client =
             std::make_unique<OAuth2ClientImpl>(cluster_manager, filter_config->oauthTokenEndpoint()); //TODO: create oauth_client per route
         callbacks.addStreamDecoderFilter(
-            std::make_shared<OAuth2Filter>(filter_config, oauth2_config, std::move(oauth_client), context.timeSource()));
+            std::make_shared<OAuth2Filter>(filter_config, oauth2_config, std::move(oauth_client), context.timeSource(),context));
       };
 }
 
