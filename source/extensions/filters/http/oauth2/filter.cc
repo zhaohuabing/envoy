627670fb3431d42d83fabec822a2ed1544606bc8
huabing zhao <zhaohuabing@gmail.com>
2023-10-04 14:14:13 +0800

    move oauth2 config out
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>

--- source/extensions/filters/http/oauth2/filter.cc
+++ source/extensions/filters/http/oauth2/filter.cc
@@ -240,7 +240,7 @@ Http::FilterHeadersStatus OAuth2Filter::decodeHeaders(Http::RequestHeaderMap& he
   const auto* oauth2_config =
       Http::Utility::resolveMostSpecificPerFilterConfig<OAuth2Config>(decoder_callbacks_);
   if (oauth2_config) {
-    OAuth2ConfigSharedPtr oauth2_config_shared(*oauth2_config);
+    OAuth2ConfigSharedPtr oauth2_config_shared(oauth2_config);
     config_->setOAuth2Config(oauth2_config_shared);
   }
 
