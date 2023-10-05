501be3a1720e84ba75fa74d7ea333daa0a507344
huabing zhao <zhaohuabing@gmail.com>
2023-10-05 13:32:24 +0800

    move oauth2 config out
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>

--- source/extensions/filters/http/oauth2/filter.cc
+++ source/extensions/filters/http/oauth2/filter.cc
@@ -231,7 +231,7 @@ bool OAuth2CookieValidator::isValid() const { return hmacIsValid() && timestampI
 OAuth2Filter::OAuth2Filter(FilterConfigSharedPtr config, OAuth2ConfigSharedPtr global_config,
                            std::unique_ptr<OAuth2Client>&& oauth_client,
                            TimeSource& time_source,Server::Configuration::FactoryContext& context)
-    : validator_(std::make_shared<OAuth2CookieValidator>(time_source, config->cookieNames())),
+    : validator_(std::make_shared<OAuth2CookieValidator>(time_source, current_config_->cookieNames())),
       oauth_client_(std::move(oauth_client)), config_(std::move(config)), global_config_(std::move(global_config)),
       time_source_(time_source), context_(context) {
 
