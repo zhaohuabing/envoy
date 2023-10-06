c3a1b1808e909bffaaf1a414e8d069981116f8a4
huabing zhao <zhaohuabing@gmail.com>
2023-10-06 09:24:00 +0800

    move oauth2 config out
    Signed-off-by: huabing zhao <zhaohuabing@gmail.com>

--- source/common/http/utility.h
+++ source/common/http/utility.h
@@ -636,7 +636,7 @@ const ConfigType* resolveMostSpecificPerFilterConfig(const Http::StreamFilterCal
   static_assert(std::is_base_of<Router::RouteSpecificFilterConfig, ConfigType>::value,
                 "ConfigType must be a subclass of Router::RouteSpecificFilterConfig");
   ASSERT(callbacks != nullptr);
-  std::out << "resolveMostSpecificPerFilterConfig XXXXXXXXXXX" << std::endl;
+  std::cout << "resolveMostSpecificPerFilterConfig XXXXXXXXXXX" << std::endl;
   return dynamic_cast<const ConfigType*>(callbacks->mostSpecificPerFilterConfig());
 }
 
