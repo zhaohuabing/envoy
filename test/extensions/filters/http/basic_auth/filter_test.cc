82a7412c27853bd2b368dc9b2dd05cb594cfcb4f
Huabing Zhao <zhaohuabing@gmail.com>
2023-10-17 08:56:51 +0000

    add more tests
    Signed-off-by: Huabing Zhao <zhaohuabing@gmail.com>

--- test/extensions/filters/http/basic_auth/filter_test.cc
+++ test/extensions/filters/http/basic_auth/filter_test.cc
@@ -16,9 +16,10 @@ namespace BasicAuth {
 
 class FilterTest : public testing::Test {
 public:
-  FilterTest() = default;
-
-  void setup(const std::vector<User>& users) {
+  FilterTest(){
+  std::vector<User> users;
+  users.push_back({"user1", "tESsBmE/yNY3lb6a0L6vVQEZNqw="}); //user1:test1
+  users.push_back({"user2", "EJ9LPFDXsN9ynSmbxvjp75Bmlx8="}); //user2:test2
     config_ =
         std::make_shared<FilterConfig>(users, "stats", *stats_.rootScope());
     filter_ = std::make_shared<BasicAuthFilter>(config_);
@@ -33,18 +34,38 @@ public:
 
 
 TEST_F(FilterTest, BasicAuth) {
-  std::vector<User> users;
-  users.push_back({"user1", "tESsBmE/yNY3lb6a0L6vVQEZNqw="});
-  users.push_back({"user2", "EJ9LPFDXsN9ynSmbxvjp75Bmlx8="});
-  setup(users);
-
+  //user1:test1	
   Http::TestRequestHeaderMapImpl request_headers_user1{{"Authorization", "Basic dXNlcjE6dGVzdDE="}};
   EXPECT_EQ(Http::FilterHeadersStatus::Continue,
             filter_->decodeHeaders(request_headers_user1, true));
 
+  //user2:test2	
   Http::TestRequestHeaderMapImpl request_headers_user2{{"Authorization", "Basic dXNlcjI6dGVzdDI="}};
   EXPECT_EQ(Http::FilterHeadersStatus::Continue,
             filter_->decodeHeaders(request_headers_user2, true));            
+}
+
+TEST_F(FilterTest, UserNotExist) {
+  //user3:test2	
+  Http::TestRequestHeaderMapImpl request_headers_user1{{"Authorization", "Basic dXNlcjM6dGVzdDI="}};
+  EXPECT_EQ(Http::FilterHeadersStatus::StopIteration,
+            filter_->decodeHeaders(request_headers_user1, true));
+
+}
+
+TEST_F(FilterTest, InvalidPassword) {
+  //user1:test2	
+  Http::TestRequestHeaderMapImpl request_headers_user1{{"Authorization", "Basic dXNlcjE6dGVzdDI="}};
+  EXPECT_EQ(Http::FilterHeadersStatus::StopIteration,
+            filter_->decodeHeaders(request_headers_user1, true));
+
+}
+
+TEST_F(FilterTest, NoAuthHeader) {
+  Http::TestRequestHeaderMapImpl request_headers_user1;
+  EXPECT_EQ(Http::FilterHeadersStatus::StopIteration,
+            filter_->decodeHeaders(request_headers_user1, true));
+
 }
 
 } // namespace BasicAuth
